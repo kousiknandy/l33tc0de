7809b6a1c71f1962354b4748633e06e6e57da824
kousiknandy <kousiknandy@users.noreply.github.com>
2025-01-19 23:16:46 +0000

    speedup minimum-moves-to-equal-array-elements-v2.py

--- minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements-v2.py
+++ minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements-v2.py
@@ -1,10 +1,9 @@
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
-        l = 0
+        l = len(nums)
         s = 0
         m = 1e10
         for n in nums:
-            l += 1
             s += n
-            m = min(m,n) 
+            m = m if m < n else n
         return s - l*m
