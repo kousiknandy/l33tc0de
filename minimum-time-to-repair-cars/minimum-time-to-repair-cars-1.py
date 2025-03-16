577119df431bc050af0a6f4633bf98f109eaec0e
kousiknandy <kousiknandy@users.noreply.github.com>
2025-03-16 18:54:26 +0000

    Update minimum-time-to-repair-cars-1.py
    CPU 78% MEM 49%

--- minimum-time-to-repair-cars/minimum-time-to-repair-cars-1.py
+++ minimum-time-to-repair-cars/minimum-time-to-repair-cars-1.py
@@ -1,15 +1,10 @@
+import math
+
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        def isqrt(n: int) -> int:
-            x = n
-            y = (x + 1) // 2
-            while y < x:
-                x = y
-                y = (x + n // x) // 2
-            return x
 
         def completes(t: int, ranks: List[int], cars: int) -> bool:
-            cc = [isqrt(t//r) for r in ranks]
+            cc = [math.isqrt(t//r) for r in ranks]
             return sum(cc) >= cars
 
         ll, m, ul = 1, 1, 1
