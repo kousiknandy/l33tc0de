d9c4b273b897da3485aec276c1acb75a2ec889e0
kousiknandy <kousiknandy@users.noreply.github.com>
2025-03-29 23:35:01 +0000

    Update max-consecutive-ones-iii.py
    CPU 5% MEM 42%

--- leetcode75/max-consecutive-ones-iii.py
+++ leetcode75/max-consecutive-ones-iii.py
@@ -1,33 +1,23 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        h,t = 0,0
-        z = 1 if nums[h] == 0 else 0
-        w = 0
-        while h < len(nums):
-            print(">",w,t,h,z)
-            while nums[h] == 1: 
+        h,t,w,z = 0,0,0,0
+        while True:
+            # print("",w,t,nums[t],h, nums[h],z)
+            if nums[h] == 1: h += 1
+            w = max(w, h-t)
+            if h >= len(nums): break
+            if z == k and nums[h] == 0 and nums[t] == 0:
                 h += 1
-                if h >= len(nums): 
-                    w = max(w, h-t)
-                    return w
-            w = max(w,h-t)
-            if z == k:
-                while nums[t] == 1: t += 1
-                while nums[t] == 0 and nums[h] == "0":
-                    t += 1
-                    h += 1
-                    if h >= len(nums): return w
-            print(">>",w,t,h,z)
-            if nums[t] == 0:
                 t += 1
-                z -= 1
-                # if z == 0 or t == h: break
-            while nums[h] == 0 and z < k:
+                if h >= len(nums): break
+            if z < k and nums[h] == 0:
                 h += 1
                 z += 1
-                if h >= len(nums):
-                    w = max(w, h-t)
-                    break
             w = max(w, h-t)
+            if h >= len(nums): break
+            # print(" ",w,t,nums[t],h, nums[h],z)
+            if z == k:
+                if nums[h] == 0:
+                    if nums[t] == 0: z -= 1
+                    t += 1
         return w
-            
