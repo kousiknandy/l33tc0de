05cd026cedc911b2c1140b10d581f2c744a9da60
kousiknandy <kousiknandy@users.noreply.github.com>
2025-04-07 22:25:50 +0100

    Update lowest-common-ancestor-of-a-binary-tree.py
    CPU 96% MEM 72%

--- leetcode75/lowest-common-ancestor-of-a-binary-tree.py
+++ leetcode75/lowest-common-ancestor-of-a-binary-tree.py
@@ -6,32 +6,13 @@
 #         self.right = None
 
 class Solution:
-    def searchpath(self, root, p, q, path):
-        if root == p: 
-            self.p = path[:]
-            self.p.append(p)
-        if root == q:
-            self.q = path[:]
-            self.q.append(q)
-        if self.p and self.q: return
-        if root.left:
-            newpath = path[:]
-            newpath.append(root)
-            self.searchpath(root.left, p, q, newpath)
-            if self.p and self.q: return
-        if root.right:
-            newpath = path[:]
-            newpath.append(root)
-            self.searchpath(root.right, p, q, newpath)
-            if self.p and self.q: return
-
+    def search_pq(self, root, p, q):
+        if root in [p,q]: return root
+        l = self.search_pq(root.left, p, q) if root.left else None
+        r = self.search_pq(root.right, p, q) if root.right else None
+        if l and r: return root
+        return l or r
 
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.p = []
-        self.q = []
-        self.searchpath(root, p, q, [])
-        ans = root
-        for pp,pq in zip(self.p, self.q):
-            if pp.val != pq.val: break
-            ans = pp
-        return ans 
+        n = self.search_pq(root,p,q)
+        return n
