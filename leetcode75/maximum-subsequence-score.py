eceab760901b6be5a1381f75a7e9a1565b0d66f0
kousiknandy <kousiknandy@users.noreply.github.com>
2025-04-19 12:15:51 +0100

    Update maximum-subsequence-score.py
    Using heapq passes another test

--- leetcode75/maximum-subsequence-score.py
+++ leetcode75/maximum-subsequence-score.py
@@ -1,13 +1,16 @@
+import heapq
+
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
         combo = sorted(zip(nums2,nums1), reverse=True)
         hip1 =[x[1] for x in combo[:k]]
+        heapq.heapify(hip1)
         min2 = combo[k-1][0]
         maxp = min2 * sum(hip1)
         for i in range(k,len(nums2)):
             min2 = combo[i][0]
-            hip1.append(combo[i][1])
-            p = min2 * sum(heapq.nlargest(k, hip1))
+            heapq.heappushpop(hip1, combo[i][1])
+            p = min2 * sum(hip1)
             # print(min2, hip1, p, maxp)
             maxp = max(maxp, p)
         return maxp
