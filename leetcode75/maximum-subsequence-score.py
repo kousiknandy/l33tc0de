688dfb2e7c83ed4c2f90c049f5d1fe0c3565fb2b
kousiknandy <kousiknandy@users.noreply.github.com>
2025-04-19 12:17:57 +0100

    Update maximum-subsequence-score.py
    CPU 97% MEM 45%

--- leetcode75/maximum-subsequence-score.py
+++ leetcode75/maximum-subsequence-score.py
@@ -6,11 +6,13 @@ class Solution:
         hip1 =[x[1] for x in combo[:k]]
         heapq.heapify(hip1)
         min2 = combo[k-1][0]
-        maxp = min2 * sum(hip1)
+        max1 = sum(hip1)
+        maxp = min2 * max1
         for i in range(k,len(nums2)):
             min2 = combo[i][0]
-            heapq.heappushpop(hip1, combo[i][1])
-            p = min2 * sum(hip1)
+            it = heapq.heappushpop(hip1, combo[i][1])
+            max1 += combo[i][1] - it
+            p = min2 * max1
             # print(min2, hip1, p, maxp)
             maxp = max(maxp, p)
         return maxp
