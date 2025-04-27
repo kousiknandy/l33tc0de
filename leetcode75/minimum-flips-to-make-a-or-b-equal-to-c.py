4c9b5134b50a0f744b3b18a5c041e36bc52ff4df
kousiknandy <kousiknandy@users.noreply.github.com>
2025-04-27 18:41:25 +0100

    Update minimum-flips-to-make-a-or-b-equal-to-c.py
    CPU 62% MEM 10%

--- leetcode75/minimum-flips-to-make-a-or-b-equal-to-c.py
+++ leetcode75/minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -1,18 +1,23 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
-        def bit(n):
-            p = 1 << 31
+        def bit(n, p):
             while p:
-                yield 1 if n & p else 0
-                p >>= 1
+                yield n % 2
+                p //= 2
+                n //= 2
             return 0
-        ba, bb, bc = bit(a), bit(b), bit(c)
+
+        ba, bb, bc = bit(a, max(a, b, c)), bit(b, max(a, b, c)), bit(c, max(a, b, c))
         flip = 0
         for c1 in bc:
             a1 = next(ba)
             b1 = next(bb)
-            if c1 and (a1 or b1): continue
-            if not c1 and (not a1 and not b1): continue
-            if c1: flip += 1
-            else: flip += 2 if (a1 and b1) else 1
+            if c1 and (a1 or b1):
+                continue
+            if not c1 and (not a1 and not b1):
+                continue
+            if c1:
+                flip += 1
+            else:
+                flip += 2 if (a1 and b1) else 1
         return flip
